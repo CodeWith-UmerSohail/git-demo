d9f7afc8681041cb69c9c26d28ada194bc8ca3ca
unknown <pe27823ncr@gmail.com>
2024-08-25 14:11:50 +0500

    update program

---
+++ marksheet.dart
@@ -0,0 +1,11 @@
+void main() {
+  int maths = 95;
+  int phys = 85;
+  int isl = 99;
+  int urdu = 86;
+  int chem = 75;
+  int obtained = maths + phys + isl + urdu + chem;
+  int totalmarks = 500;
+  double percntage = obtained / totalmarks * 100;
+  print("$percntage%");
+}
