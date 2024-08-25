d6506b36d7e46cfdfab6ad1d262c6fe9b07075b2
unknown <pe27823ncr@gmail.com>
2024-08-25 13:32:29 +0500

    gitprc

---
+++ prc.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Student's name
+  String name = "Robert";
+
+  // Marks in three subjects
+  int marks1 = 78;
+  int marks2 = 45;
+  int marks3 = 62;
+
+  // Calculate total marks
+  int totalMarks = marks1 + marks2 + marks3;
+
+  // Calculate percentage
+  double percentage = (totalMarks / 300) * 100;
+
+  // Print the results
+  print("Name: $name");
+  print("Marks in Subject 1: $marks1");
+  print("Marks in Subject 2: $marks2");
+  print("Marks in Subject 3: $marks3");
+  print("Total Marks: $totalMarks");
+  print("Percentage: $percentage%");
+}
